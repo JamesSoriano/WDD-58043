45b98536b36bdf64b8ebf2971eabab18c7d16f02
James Harry Soriano <89111597+JamesSoriano@users.noreply.github.com>
2022-11-18 22:28:57 +0800

    Add files via upload

---
+++ login.php
@@ -0,0 +1,39 @@
+<html>
+<head>    
+<title> My Login Page  </title>
+<body bgcolor = "00CED1">
+<form action = "connection.php" method = "post">
+
+	<center>
+	<p>
+		<font color= "Yellow" face = 'times new roman'>
+		<label>Username:</label>
+		<input type ="text" name = "user"/>
+		</font>
+	</p>
+	<p>
+		<font color= "Yellow" face = 'times new roman'>
+		<label>Password:</label>
+		<input type ="password" name = "text"/>
+		</font>
+	</p>
+	<p>
+	
+		<input type = "submit" name = "Login"/>
+	</p>
+	</center>
+</form>
+</body>
+</head>
+<?php
+
+
+
+
+?>
+
+
+
+
+
+</html>
